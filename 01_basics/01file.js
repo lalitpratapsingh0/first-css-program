5851f934d42154c56fd1435bee777824da1c65e4
lalit pratap singh <lpslalit3436@gmail.com>
2025-09-08 10:11:20 +0000

    just opening

--- 01_basics/01file.js
+++ 01_basics/01file.js
@@ -1,1 +1,29 @@
-console.log("Kunal");
+// // console.log("Kunal");
+// // let namee = prompt("Enter your name:");
+// // console.log("Hello, " + namee);
+
+// let firstNumber = "2";
+// let secondNumber = 4;
+
+// console.log(firstNumber + secondNumber); // Concatenation
+
+let value = 2/3
+// console.log(value);
+
+
+let num1 = "2"
+let num2 = 2
+
+let renueNum1 = parseInt(num1)
+// console.log(renueNum1+num2)
+
+let obj = {
+    name: "Kunal",
+    age: 25,
+    Height: 5.7
+};
+
+console.log(Object.keys); // 5.7
+console.log(Object.values(obj)); 
+  // "Height"
+
